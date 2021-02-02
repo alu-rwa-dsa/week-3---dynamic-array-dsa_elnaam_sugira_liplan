9dcfbe9d3cfc6e947381bc06067d9f882f0de3a3
Elnaam10 <37862623+Elnaam10@users.noreply.github.com>
2021-02-02 13:55:15 -0800

    Update test_question4.py

--- test_question4.py
+++ test_question4.py
@@ -5,21 +5,21 @@ from question4 import asso_list
 class TestQuestion4(unittest.TestCase):
     def test_first(self):
         p = asso_list()
-        self.assertEqual(p.add("meonly", 19, "CSS", "UTK"), {'name': 'meonly', 'age': 19, 'major': 'CSS', 'country': 'UTK'})
+        self.assertEqual(p.add("liplan", 19, "CSS", "UTf"), {'name': 'liplan', 'age': 20, 'major': 'CSS', 'country': 'KE'})
 
     def test_second(self):
         p = asso_list()
-        p.add("meonly", 19, "CSS", "UTK")
-        self.assertEqual(p.a_remove("age"), {'name': 'meonly', 'major': 'CSS', 'country': 'UTK'})
+        p.add("liplan", 19, "CSS", "KE")
+        self.assertEqual(p.a_remove("age"), {'name': 'liplan', 'major': 'CSS', 'country': 'KE'})
 
     def test_third(self):
         p = asso_list()
-        p.add("meonly", 19, "CSS", "UTK")
-        self.assertEqual(p.a_modify("meonly2", "CSS", "UTK"), {'name': 'meonly2', 'age' : 19, 'major': 'CSS', 'country': 'UTK'})
+        p.add("liplan", 19, "CSS", "KE")
+        self.assertEqual(p.a_modify("liplan", "CSS", "KE"), {'name': 'liplan', 'age' : 19, 'major': 'CSS', 'country': 'KE'})
 
     def test_fourth(self):
         p = asso_list()
-        p.add("meonly", 89, "19", "UTK")
+        p.add("liplan", 89, "19", "KE")
         self.assertEqual(p.a_lookup("age"), "not found")
 
 
