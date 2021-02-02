6bdc0d00f7f80c155faa4bdbfdcc2fce04789b09
lekipising <leeplanke@gmail.com>
2021-02-02 22:24:29 +0200

    updates

--- testquestion2.py
+++ testquestion2.py
@@ -11,17 +11,19 @@ class testq2(unittest.TestCase):
     def test_c2(self):
         ardy1 = DynamicArray()
         ardy1.add(57)
-        self.assertEqual(ardy1.__get__(0), 57)   
+        self.assertEqual(ardy1.__len__(), 1)
 
     def test_c3(self):
         ardy1 = DynamicArray()
         ardy1.add(57)
-        self.assertEqual(ardy1.__get__(0), 57)
+        ardy1.add(7)
+        self.assertEqual(ardy1.__len__(), 2)
 
     def test_c4(self):
         ardy1 = DynamicArray()
         ardy1.add(57)
-        self.assertEqual(ardy1.__get__(0), 57)
+        ardy1.dele()
+        self.assertEqual(ardy1.__len__(), 0)
 
 
 if __name__ == "__main__":
